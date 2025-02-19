d36b85abd0f1a24683b630023938a222d8d26187
Vimal Seshadri Raguraman <62070888+Vimal-Seshadri-Raguraman@users.noreply.github.com>
2025-02-19 01:11:37 -0500

    Delete FMP directory

--- FMP/fmp_websockets/forex_websocket.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from base_websocket_client import BaseWebsocketClient
-
-class ForexWebsockets(BaseWebsocketClient):
-    def __init__(self, pairs: List[str], api_key: str, 
-                 uri: str = "wss://forex.financialmodelingprep.com") -> None:
-        super().__init__(pairs, api_key, uri)
